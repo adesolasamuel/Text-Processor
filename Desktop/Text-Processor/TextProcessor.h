da1530367d47440e88c6b80c47d49a90b6b52704
Dochie <dochieonaji@gmail.com>
2023-03-14 14:30:43 +0100

    Implemented the functionality for lowercase conversion

---
+++ Desktop/Text-Processor/TextProcessor.h
@@ -0,0 +1,29 @@
+//
+// 
+//Contributors: Samuel Adesola, Adoche Onaji
+
+#pragma once
+
+
+class TextProcessor {
+
+private:
+	int option;
+
+public:
+	std::string filePath ;   //File Path to the text file
+	std::string words;      //A string to hold the output, Check TextProcessor.cpp to how it was used
+	std::string filename;
+	
+
+	int getOption() { return option; };       //An Inline function returning the value in option
+	void priliminary();
+	void makeAllCaps();
+	void makeAllSmall();
+
+	void AppInfo();
+
+};
+
+
+
